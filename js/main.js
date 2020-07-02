edb892a75be641258bfbee174fd4681953eb4c62
Tamas <t.tomi@ent.hu>
2020-07-02 21:33:33 +0200

    jawa added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-console.log("hello, this is a console message");
+function calcAmount() {
+let price = 1000;
+let amountinput = document. querySelector("input[name='amount-input']");
+let amount = parseInt (amountinput.value)*price;
+alert("Fizetendő:"+ amount + "Ft");
+}
