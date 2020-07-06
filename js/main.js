6063a9dc991f72c4434b1567dd6b80348d5bbb86
Tamas <t.tomi@ent.hu>
2020-07-06 21:55:55 +0200

    jawa form added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 function calcAmount() {
 let price = 1000;
 let amountinput = document. querySelector("input[name='amount-input']");
+let showAmount = document.querySelector("span.show-amount");
 let amount = parseInt (amountinput.value)*price;
-alert("Fizetendő:"+ amount + "Ft");
+
+
+showAmount.innerHTML = amount;
+
 }
