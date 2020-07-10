e6f2e72b2ae50ea4a2bce9aa1ee573966f2609ea
Tamas <t.tomi@ent.hu>
2020-07-10 22:10:35 +0200

    file adedd

--- js/main.js
+++ js/main.js
@@ -2,9 +2,18 @@ function calcAmount() {
 let price = 1000;
 let amountinput = document. querySelector("input[name='amount-input']");
 let showAmount = document.querySelector("span.show-amount");
-let amount = parseInt (amountinput.value)*price;
+let amountNumber= parseInt(amountinput.value);
+amountNumber= isNaN(amountNumber) ? 0 : amountNumber;
 
 
+
+if (amountNumber > 10) {
+    alert("Maximum 10 termék rendelhető!");
+
+ } else if (amountNumber < 1) {
+alert("minimum 1 terméket rendelhet!");
+ }  else {
+     let amount = parseInt (amountinput.value)*price;
 showAmount.innerHTML = amount;
-
+}
 }
